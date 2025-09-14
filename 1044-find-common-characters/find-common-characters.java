fda577ff715d3d9d5547484ba1aca36a92cb9bd8
Prashanth Kumar 👨🏽‍💻 <146945037+Pprashanthkumar@users.noreply.github.com>
2025-09-14 17:49:35 +0530

    Time: 4 ms (48.21%) | Memory: 43.3 MB (54.04%) - LeetSync

--- 1044-find-common-characters/find-common-characters.java
+++ 1044-find-common-characters/find-common-characters.java
@@ -3,24 +3,19 @@ class Solution {
         List<String> res=new ArrayList<>();
         int main[]=new int[26];
         String firstWord=words[0];
-        int ind=0;
         for(char ch:firstWord.toCharArray()){
             main[ch-'a']++;
         }
-        // System.out.println(Arrays.toString(main));
 
         for(String curStr:words){
             int cur[]=new int[26];
             for(char ch:curStr.toCharArray()){
                 cur[ch-'a']++;
             }
-            System.out.println("Ma:"+Arrays.toString(main));
+            
             for(int i=0;i<26;i++){
                 main[i]=Math.min(main[i],cur[i]);
             }
-            System.out.println("Cu:"+Arrays.toString(main));
-            System.out.println();
-
         }
 
         List<String> lst=new ArrayList<>();
