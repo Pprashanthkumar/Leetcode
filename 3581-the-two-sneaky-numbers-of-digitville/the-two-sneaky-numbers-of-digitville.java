502b71166145ebacaee1bc947034816b99bbd8af
Prashanth Kumar 👨🏽‍💻 <146945037+Pprashanthkumar@users.noreply.github.com>
2025-10-31 08:35:47 +0530

    Time: 3 ms (36.86%) | Memory: 44.8 MB (43.32%) - LeetSync

--- 3581-the-two-sneaky-numbers-of-digitville/the-two-sneaky-numbers-of-digitville.java
+++ 3581-the-two-sneaky-numbers-of-digitville/the-two-sneaky-numbers-of-digitville.java
@@ -1,19 +1,18 @@
 class Solution {
     public int[] getSneakyNumbers(int[] nums) {
-        
+        Arrays.sort(nums);
         List<Integer> lst=new ArrayList<>();
-        for(int i=0;i<nums.length;i++){
-            for(int j=i+1;j<nums.length;j++){
-                if(nums[i]==nums[j]) lst.add(nums[i]);
-            }
+        for(int i=0;i<nums.length-1;i++){
+            if(nums[i]==nums[i+1]) lst.add(nums[i]);
         }
-        System.out.println(lst);
-        Collections.sort(lst);
+        // System.out.println("List "+lst);
+
         int res[]=new int[lst.size()];
         for(int i=0;i<lst.size();i++){
             res[i]=lst.get(i);
         }
         return res;
+
         
     }
 }
