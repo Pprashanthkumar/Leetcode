1ef1648c5ff0cc3619d74fc8253cba4910ebc447
Prashanth Kumar 👨🏽‍💻 <146945037+Pprashanthkumar@users.noreply.github.com>
2025-10-31 08:21:17 +0530

    Time: 7 ms (3.83%) | Memory: 44.9 MB (43.32%) - LeetSync

--- 3581-the-two-sneaky-numbers-of-digitville/the-two-sneaky-numbers-of-digitville.java
+++ 3581-the-two-sneaky-numbers-of-digitville/the-two-sneaky-numbers-of-digitville.java
@@ -1,17 +1,14 @@
 class Solution {
     public int[] getSneakyNumbers(int[] nums) {
-        Map<Integer,Integer> map=new HashMap<>();
-        for(int i=0;i<nums.length;i++){
-            map.put(nums[i],map.getOrDefault(nums[i],0)+1);
-        }
-        // System.out.println("Map "+map);
         
         List<Integer> lst=new ArrayList<>();
-        for(Map.Entry<Integer,Integer> m:map.entrySet()){
-            if(m.getValue()>1) lst.add(m.getKey());
+        for(int i=0;i<nums.length;i++){
+            for(int j=i+1;j<nums.length;j++){
+                if(nums[i]==nums[j]) lst.add(nums[i]);
+            }
         }
-        // System.out.println("List "+lst);
-
+        System.out.println(lst);
+        Collections.sort(lst);
         int res[]=new int[lst.size()];
         for(int i=0;i<lst.size();i++){
             res[i]=lst.get(i);
